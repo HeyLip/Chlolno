80834f99b68d559be73496f1f002df73f2f47835
Junhyeon <wnsgus313@gmail.com>
2022-05-20 21:06:25 +0900

    챔피언 name, id

---
+++ lib/LeagueStuff/champ.dart
@@ -0,0 +1,569 @@
+/// With this custom-made function it is possible to show the name
+/// of any given champion id
+String getChampNameByID(int? champId) {
+  String champName = "";
+
+  if (champId == 266) {
+    champName = "아트록스";
+  }
+  if (champId == 103) {
+    champName = "아리";
+  }
+  if (champId == 84) {
+    champName = "아칼리";
+  }
+  if (champId == 166) {
+    champName = "아크샨";
+  }
+  if (champId == 12) {
+    champName = "알리스타";
+  }
+  if (champId == 32) {
+    champName = "아무무";
+  }
+  if (champId == 34) {
+    champName = "애니비아";
+  }
+  if (champId == 1) {
+    champName = "애니";
+  }
+  if (champId == 523) {
+    champName = "아펠리오스";
+  }
+  if (champId == 22) {
+    champName = "애쉬";
+  }
+  if (champId == 136) {
+    champName = "아우렐리온 솔";
+  }
+  if (champId == 268) {
+    champName = "아지르";
+  }
+  if (champId == 432) {
+    champName = "바드";
+  }
+  if (champId == 53) {
+    champName = "블리츠크랭크";
+  }
+  if (champId == 63) {
+    champName = "브랜드";
+  }
+  if (champId == 201) {
+    champName = "브라움";
+  }
+  if (champId == 51) {
+    champName = "케이틀린";
+  }
+  if (champId == 164) {
+    champName = "카밀";
+  }
+  if (champId == 69) {
+    champName = "카시오페아";
+  }
+  if (champId == 31) {
+    champName = "초가스";
+  }
+  if (champId == 42) {
+    champName = "코르키";
+  }
+  if (champId == 122) {
+    champName = "다리우스";
+  }
+  if (champId == 131) {
+    champName = "다이애나";
+  }
+  if (champId == 119) {
+    champName = "드레이븐";
+  }
+  if (champId == 36) {
+    champName = "문도박사";
+  }
+  if (champId == 245) {
+    champName = "에코";
+  }
+  if (champId == 60) {
+    champName = "엘리스";
+  }
+  if (champId == 28) {
+    champName = "이블린";
+  }
+  if (champId == 81) {
+    champName = "이즈리얼";
+  }
+  if (champId == 9) {
+    champName = "피들스틱";
+  }
+  if (champId == 114) {
+    champName = "피오라";
+  }
+  if (champId == 105) {
+    champName = "피즈";
+  }
+  if (champId == 3) {
+    champName = "갈리오";
+  }
+  if (champId == 41) {
+    champName = "갱플랭크";
+  }
+  if (champId == 86) {
+    champName = "가렌";
+  }
+  if (champId == 150) {
+    champName = "나르";
+  }
+  if (champId == 79) {
+    champName = "그라가스";
+  }
+  if (champId == 104) {
+    champName = "그레이브즈";
+  }
+  if (champId == 887) {
+    champName = "그웬";
+  }
+  if (champId == 120) {
+    champName = "헤카림";
+  }
+  if (champId == 74) {
+    champName = "하이머딩거";
+  }
+  if (champId == 420) {
+    champName = "일라오이";
+  }
+  if (champId == 39) {
+    champName = "이렐리아";
+  }
+  if (champId == 427) {
+    champName = "아이번";
+  }
+  if (champId == 40) {
+    champName = "잔나";
+  }
+  if (champId == 59) {
+    champName = "자르반 4세";
+  }
+  if (champId == 24) {
+    champName = "잭스";
+  }
+  if (champId == 126) {
+    champName = "제이스";
+  }
+  if (champId == 202) {
+    champName = "진";
+  }
+  if (champId == 222) {
+    champName = "징크스";
+  }
+  if (champId == 145) {
+    champName = "카이사";
+  }
+  if (champId == 429) {
+    champName = "칼리스타";
+  }
+  if (champId == 43) {
+    champName = "카르마";
+  }
+  if (champId == 30) {
+    champName = "카서스";
+  }
+  if (champId == 38) {
+    champName = "카사딘";
+  }
+  if (champId == 55) {
+    champName = "카타리나";
+  }
+  if (champId == 10) {
+    champName = "케일";
+  }
+  if (champId == 141) {
+    champName = "케인";
+  }
+  if (champId == 85) {
+    champName = "케넨";
+  }
+  if (champId == 121) {
+    champName = "카직스";
+  }
+  if (champId == 203) {
+    champName = "킨드레드";
+  }
+  if (champId == 240) {
+    champName = "클리드";
+  }
+  if (champId == 96) {
+    champName = "코그모";
+  }
+  if (champId == 7) {
+    champName = "르블랑";
+  }
+  if (champId == 64) {
+    champName = "리신";
+  }
+  if (champId == 89) {
+    champName = "레오나";
+  }
+  if (champId == 876) {
+    champName = "릴리아";
+  }
+  if (champId == 127) {
+    champName = "리산드라";
+  }
+  if (champId == 236) {
+    champName = "루시안";
+  }
+  if (champId == 117) {
+    champName = "룰루";
+  }
+  if (champId == 99) {
+    champName = "럭스";
+  }
+  if (champId == 54) {
+    champName = "말파이트";
+  }
+  if (champId == 90) {
+    champName = "말자하";
+  }
+  if (champId == 57) {
+    champName = "마오카이";
+  }
+  if (champId == 11) {
+    champName = "마스터이";
+  }
+  if (champId == 21) {
+    champName = "미스포춘";
+  }
+  if (champId == 62) {
+    champName = "오공";
+  }
+  if (champId == 82) {
+    champName = "모데카이저";
+  }
+  if (champId == 25) {
+    champName = "모르가나";
+  }
+  if (champId == 267) {
+    champName = "나미";
+  }
+  if (champId == 75) {
+    champName = "나서스";
+  }
+  if (champId == 111) {
+    champName = "노틸러스";
+  }
+  if (champId == 518) {
+    champName = "니코";
+  }
+  if (champId == 76) {
+    champName = "니달리";
+  }
+  if (champId == 56) {
+    champName = "녹턴";
+  }
+  if (champId == 20) {
+    champName = "누누";
+  }
+  if (champId == 2) {
+    champName = "올라프";
+  }
+  if (champId == 61) {
+    champName = "오리아나";
+  }
+  if (champId == 516) {
+    champName = "오른";
+  }
+  if (champId == 80) {
+    champName = "판테온";
+  }
+  if (champId == 78) {
+    champName = "뽀삐";
+  }
+  if (champId == 555) {
+    champName = "파이크";
+  }
+  if (champId == 246) {
+    champName = "키아나";
+  }
+  if (champId == 133) {
+    champName = "퀸";
+  }
+  if (champId == 497) {
+    champName = "라칸";
+  }
+  if (champId == 33) {
+    champName = "람머스";
+  }
+  if (champId == 421) {
+    champName = "렉사이";
+  }
+  if (champId == 526) {
+    champName == "렐";
+  }
+  if (champId == 888){
+    champName = "레나타";
+  }
+  if (champId == 58) {
+    champName = "레넥톤";
+  }
+  if (champId == 107) {
+    champName = "렝가";
+  }
+  if (champId == 92) {
+    champName = "리븐";
+  }
+  if (champId == 68) {
+    champName = "럼블";
+  }
+  if (champId == 13) {
+    champName = "라이즈";
+  }
+  if (champId == 360) {
+    champName = "사미라";
+  }
+  if (champId == 113) {
+    champName = "세주아니";
+  }
+  if (champId == 235) {
+    champName = "세나";
+  }
+  if (champId == 147) {
+    champName = "세라핀";
+  }
+  if (champId == 875) {
+    champName = "세트";
+  }
+  if (champId == 35) {
+    champName = "샤코";
+  }
+  if (champId == 98) {
+    champName = "쉔";
+  }
+  if (champId == 102) {
+    champName = "쉬바나";
+  }
+  if (champId == 27) {
+    champName = "신지드";
+  }
+  if (champId == 14) {
+    champName = "사이온";
+  }
+  if (champId == 15) {
+    champName = "시비르";
+  }
+  if (champId == 72) {
+    champName = "스카너";
+  }
+  if (champId == 37) {
+    champName = "소나";
+  }
+  if (champId == 16) {
+    champName = "소라카";
+  }
+  if (champId == 50) {
+    champName = "스웨인";
+  }
+  if (champId == 517) {
+    champName = "사일러스";
+  }
+  if (champId == 134) {
+    champName = "신드라";
+  }
+  if (champId == 223) {
+    champName = "탐켄치";
+  }
+  if (champId == 163) {
+    champName = "탈리야";
+  }
+  if (champId == 91) {
+    champName = "탈론";
+  }
+  if (champId == 44) {
+    champName = "타릭";
+  }
+  if (champId == 17) {
+    champName = "티모";
+  }
+  if (champId == 412) {
+    champName = "쓰레쉬";
+  }
+  if (champId == 18) {
+    champName = "트리스타나";
+  }
+  if (champId == 48) {
+    champName = "트런들";
+  }
+  if (champId == 23) {
+    champName = "트린다미어";
+  }
+  if (champId == 4) {
+    champName = "트위스티드 페이트";
+  }
+  if (champId == 29) {
+    champName = "트위치";
+  }
+  if (champId == 77) {
+    champName = "우디르";
+  }
+  if (champId == 6) {
+    champName = "우르곳";
+  }
+  if (champId == 110) {
+    champName = "바루스";
+  }
+  if (champId == 67) {
+    champName = "베인";
+  }
+  if (champId == 45) {
+    champName = "베이가";
+  }
+  if (champId == 161) {
+    champName = "벨코즈";
+  }
+  if (champId == 711 ) {
+    champName = "벡스";
+  }
+  if (champId == 254) {
+    champName = "바이";
+  }
+  if (champId == 234) {
+    champName = "비에고";
+  }
+  if (champId == 112) {
+    champName = "빅토르";
+  }
+  if (champId == 8) {
+    champName = "블라디미르";
+  }
+  if (champId == 106) {
+    champName = "볼리베어";
+  }
+  if (champId == 19) {
+    champName = "워윅";
+  }
+  if (champId == 498) {
+    champName = "자야";
+  }
+  if (champId == 101) {
+    champName = "제라스";
+  }
+  if (champId == 5) {
+    champName = "신짜오";
+  }
+  if (champId == 157) {
+    champName = "야스오";
+  }
+  if (champId == 777) {
+    champName = "요네";
+  }
+  if (champId == 83) {
+    champName = "요릭";
+  }
+  if (champId == 350) {
+    champName = "유미";
+  }
+  if (champId == 154) {
+    champName = "자크";
+  }
+  if (champId == 238) {
+    champName = "제드";
+  }
+  if (champId == 221) {
+    champName = "제리";
+  }
+  if (champId == 115) {
+    champName = "직스";
+  }
+  if (champId == 26) {
+    champName = "질리언";
+  }
+  if (champId == 142) {
+    champName = "조이";
+  }
+  if (champId == 143) {
+    champName = "자이라";
+  }
+  if (champName == "") {
+    //print("Champion not available, ID: $champId");
+    champName = 'Not available';
+  }
+  return champName;
+}
+
+int getChampIDByName(String champName) {
+  var champNames = getChampNames();
+  var idx = champNames.indexOf(champName);
+  var champIDs = getChampIDs();
+  return champIDs[idx];
+}
+
+List<String> getChampNames() {
+  return [
+    'Aatrox',       'Ahri',       'Akali',       'Akshan',     'Alistar',
+    'Amumu',        'Anivia',     'Annie',       'Aphelios',   'Ashe',
+    'AurelionSol',  'Azir',       'Bard',        'Blitzcrank', 'Brand',
+    'Braum',        'Caitlyn',    'Camille',     'Cassiopeia', 'Chogath',
+    'Corki',        'Darius',     'Diana',       'Draven',     'DrMundo',
+    'Ekko',         'Elise',      'Evelynn',     'Ezreal',     'Fiddlesticks',
+    'Fiora',        'Fizz',       'Galio',       'Gangplank',  'Garen',
+    'Gnar',         'Gragas',     'Graves',      'Gwen',       'Hecarim',
+    'Heimerdinger', 'Illaoi',     'Irelia',      'Ivern',      'Janna',
+    'JarvanIV',     'Jax',        'Jayce',       'Jhin',       'Jinx',
+    'Kaisa',        'Kalista',    'Karma',       'Karthus',    'Kassadin',
+    'Katarina',     'Kayle',      'Kayn',        'Kennen',     'Khazix',
+    'Kindred',      'Kled',       'KogMaw',      'Leblanc',    'LeeSin',
+    'Leona',        'Lillia',     'Lissandra',   'Lucian',     'Lulu',
+    'Lux',          'Malphite',   'Malzahar',    'Maokai',     'MasterYi',
+    'MissFortune',  'MonkeyKing', 'Mordekaiser', 'Morgana',    'Nami',
+    'Nasus',        'Nautilus',   'Neeko',       'Nidalee',    'Nocturne',
+    'Nunu',         'Olaf',       'Orianna',     'Ornn',       'Pantheon',
+    'Poppy',        'Pyke',       'Qiyana',      'Quinn',      'Rakan',
+    'Rammus',       'RekSai',     'Rell',        'Renata',     'Renekton',
+    'Rengar',       'Riven',      'Rumble',      'Ryze',       'Samira',
+    'Sejuani',      'Senna',      'Seraphine',   'Sett',       'Shaco',
+    'Shen',         'Shyvana',    'Singed',      'Sion',       'Sivir',
+    'Skarner',      'Sona',       'Soraka',      'Swain',      'Sylas',
+    'Syndra',       'TahmKench',  'Taliyah',     'Talon',      'Taric',
+    'Teemo',        'Thresh',     'Tristana',    'Trundle',    'Tryndamere',
+    'TwistedFate',  'Twitch',     'Udyr',        'Urgot',      'Varus',
+    'Vayne',        'Veigar',     'Velkoz',      'Vex',        'Vi',
+    'Viego',        'Viktor',     'Vladimir',    'Volibear',   'Warwick',
+    'Xayah',        'Xerath',     'XinZhao',     'Yasuo',      'Yone',
+    'Yorick',       'Yuumi',      'Zac',         'Zed',        'Zeri',
+    'Ziggs',        'Zilean',     'Zoe',         'Zyra'
+  ];
+}
+
+List<int> getChampIDs() {
+  return [
+    266, 103, 84, 12, 32,
+    32, 34, 1, 523, 22,
+    136, 268, 432, 53, 63,
+    201, 51, 164, 69, 31,
+    42, 122, 131, 119, 36,
+    245, 60, 28, 81, 9,
+    114, 105, 3, 41, 86,
+    150, 79, 104, 887, 120,
+    74, 420, 39, 427, 40,
+    59, 24, 126, 202, 222,
+    145, 429, 43, 30, 38,
+    55, 10, 141, 85, 121,
+    203, 240, 96, 7, 64,
+    89, 976, 127, 236, 117,
+    99, 54, 90, 57, 11,
+    21, 62, 82, 25, 267,
+    75, 111, 518, 76, 56,
+    20, 2, 61, 516, 80,
+    78, 555, 246, 133, 497,
+    33, 421, 526, 888, 58,
+    107, 92, 68, 13, 360,
+    113, 235, 147, 875, 35,
+    98, 102, 27, 14, 15,
+    72, 37, 16, 50, 517,
+    134, 223, 163, 91, 44,
+    17, 412, 18, 48, 23,
+    4, 29, 77, 6, 110,
+    67, 45, 161, 711, 254,
+    234, 112, 8, 106, 19,
+    498, 101, 5, 157, 777,
+    83, 350, 154, 238, 221,
+    115, 26, 142, 143
+  ];
+}
